c8cda31db07034152892a882b8b6aea1889e9e72
Justin Cooksey <justin@cooksey.com.au>
2018-01-31 20:11:01 +1100

    w

---
+++ TravelRecordApp/HomePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    public partial class HomePage : TabbedPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        void ToolbarAdd_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new NewTravelPage());
+        }
+    }
+}
---
+++ TravelRecordApp/TravelRecordAppPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+
+namespace TravelRecordApp
+{
+    public partial class TravelRecordAppPage : ContentPage
+    {
+        public TravelRecordAppPage()
+        {
+            InitializeComponent();
+        }
+
+        void LoginButton_Clicked(object sender, System.EventArgs e)
+        {
+            bool isEmailEmpty = string.IsNullOrEmpty(emailEntry.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(passwordEntry.Text);
+
+            if (isEmailEmpty || isPasswordEmpty)
+            {
+                
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+
+        }
+
+    }
+}
